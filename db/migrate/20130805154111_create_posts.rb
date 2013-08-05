579f3d21a1382e6fb7576d0606cd4e14a7d248ec
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 11:41:12 -0500

    Remove email from post table

--- db/migrate/20130805154111_create_posts.rb
+++ db/migrate/20130805154111_create_posts.rb
@@ -3,7 +3,6 @@ class CreatePosts < ActiveRecord::Migration
     create_table :posts do |t|
       t.string :title
       t.string :description
-      t.string :email
       t.integer :price
       t.belongs_to :category
       t.belongs_to :user
