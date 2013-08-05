6b6a353e99f17ec99438e74308be6f7738130123
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-05 16:13:18 -0500

    Add actions for User and view for create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,20 @@
 class UsersController < ApplicationController
 
+  def new
+    @user = User.new
+  end
 
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      @errors = @user.errors.full_messages
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
 
 end
